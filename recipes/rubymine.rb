330a52759e0c10fc1daf95a35fcafd609a9274d4
Renlor <Renlor@liddev.com>
2015-09-02 23:39:08 -0600

    Specified version, correct remote url, and version for rubymine.

--- recipes/rubymine.rb
+++ recipes/rubymine.rb
@@ -1,5 +1,8 @@
 include_recipe 'workstation::user_ensign'
 
 node.override['rubymine']['user'] = 'ensign'
+node.override['rubymine']['base_url'] = 'http://download-cf.jetbrains.com/ruby'
+node.override['rubymine']['version'] = '7.1.4'
+node.override['rubymine']['checksum'] = '9b5aa6099bca2dc6a1eefd1633ef6d0424a8767c95ca189bc18774a5222b8b43'
 
-include_recipe 'rubymine'
+include_recipe 'rubymine::default'
