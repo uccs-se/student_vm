d23be3d92615812961f99afd20cb567606785e49
Renlor <Renlor@liddev.com>
2015-09-07 18:10:45 -0600

    Added full path to knife program call.

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name             'workstation'
-version           '0.3.8'
+version           '0.3.9'
 maintainer       'Renlor'
 maintainer_email 'jvandebr@uccs.edu'
 description      'Configure a student vm with the software necessary for the UCCS Software Engineering course.'
--- recipes/update.rb
+++ recipes/update.rb
@@ -17,11 +17,11 @@ cron 'update' do
   user    node['chef']['user']
   shell   node['chef']['shell']
   command %W{
-         git clone -b dev #{node['git']['repo']} #{node['chef']['repo']} &&
+         /usr/bin/git clone -b dev #{node['git']['repo']} #{node['chef']['repo']} &&
          cd #{node['chef']['repo']} && /usr/bin/berks install &&
          echo -e 'y\\n' | ssh-keygen -b 2048 -t rsa -N "" -f #{node['chef']['home']}/.ssh/tmp &&
          cat #{node['chef']['home']}/.ssh/tmp.pub > #{node['chef']['home']}/.ssh/authorized_keys &&
-         knife solo cook chef@localhost  --identity-file #{node['chef']['home']}/.ssh/tmp --run-list "role[workstation]" &&
+         /opt/chefdk/bin/knife solo cook chef@localhost  --identity-file #{node['chef']['home']}/.ssh/tmp --run-list "role[workstation]" &&
          rm #{node['chef']['home']}/.ssh/authorized_keys #{node['chef']['home']}/.ssh/tmp #{node['chef']['home']}/.ssh/tmp.pub &&
          rm -rf #{node['chef']['repo']}
           }.join(' ')
