4130a3f8bf71f1e18cdc5271e87536d3b7653b60
Renlor <jvandebr@gmail.com>
2015-09-13 19:12:47 -0600

    Version bump.

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name             'workstation'
-version          '0.3.14'
+version          '0.3.15'
 maintainer       'Renlor'
 maintainer_email 'jvandebr@uccs.edu'
 description      'Configure a student vm with the software necessary for the UCCS Software Engineering course.'
