5b4746e62da88907a93168ec633efdb0a5ad30a0
Renlor <jvandebr@gmail.com>
2015-09-13 23:06:23 -0600

    Disable systemctl changes.

--- recipes/rdp.rb
+++ recipes/rdp.rb
@@ -1,20 +1,20 @@
 package 'xrdp'
 
-cookbook_file '/lib/systemd/system/xrdp.service' do
-  source 'xrdp/xrdp.service'
-  user   'root'
-  group  'root'
-  mode   00644
-end
-
-cookbook_file '/lib/systemd/system/xrdp-sesman.service' do
-  source 'xrdp/xrdp-sesman.service'
-  user   'root'
-  group  'root'
-  mode   00644
-end
-
-execute 'sudo systemctl daemon-reload'
+# cookbook_file '/lib/systemd/system/xrdp.service' do
+#   source 'xrdp/xrdp.service'
+#   user   'root'
+#   group  'root'
+#   mode   00644
+# end
+#
+# cookbook_file '/lib/systemd/system/xrdp-sesman.service' do
+#   source 'xrdp/xrdp-sesman.service'
+#   user   'root'
+#   group  'root'
+#   mode   00644
+# end
+#
+# execute 'sudo systemctl daemon-reload'
 
 service 'xrdp' do
   action [ :enable, :start ]
