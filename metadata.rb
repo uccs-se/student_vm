7ed4bcfb1d5a98a936f771f41bf05eb6c6754804
Renlor <jvandebr@gmail.com>
2015-09-13 22:45:51 -0600

    Selected correct env file for xrdp-sesman

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name             'workstation'
-version          '0.3.20'
+version          '0.3.21'
 maintainer       'Renlor'
 maintainer_email 'jvandebr@uccs.edu'
 description      'Configure a student vm with the software necessary for the UCCS Software Engineering course.'
