841b2a5ffe7ee482b4e80e43216e53a2a3ec6ffc
Renlor <jvandebr@gmail.com>
2015-09-27 22:21:56 -0600

    Update xfce.rb

--- recipes/xfce.rb
+++ recipes/xfce.rb
@@ -3,7 +3,7 @@ include_recipe 'xfce4::default'
 
 ruby_block "insert_line" do
   block do
-    file = Chef::Util::FileEdit.new("/home/ensign/.config/xfce/terminal/terminalrc")
+    file = Chef::Util::FileEdit.new("/home/ensign/.config/xfce4/terminal/terminalrc")
     file.search_file_replace_line("/CommandLoginShell=FALSE/", "CommandLoginShell=TRUE")
     file.insert_line_if_no_match("/CommandLoginShell=TRUE/", "CommandLoginShell=TRUE")
     file.write_file
