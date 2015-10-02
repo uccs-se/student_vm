a5fe446cf50227360a6df87a109fa80bcd7591ca
Renlor <jvandebr@gmail.com>
2015-10-01 23:08:09 -0600

    Update knife_solo.rb

--- recipes/knife_solo.rb
+++ recipes/knife_solo.rb
@@ -1,5 +1,5 @@
 
 execute 'knife_solo' do 
-  command 'sudo /opt/chefdk/bin/chef gem install knife-solo' 
+  command "sudo su chef -l -c 'bash -i /opt/chefdk/bin/chef gem install knife-solo'"
 end 
 
