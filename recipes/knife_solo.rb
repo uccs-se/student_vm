7823831b0ff76ee278641cb72c96aa09dfc906e5
Renlor <jvandebr@gmail.com>
2015-10-04 11:13:24 -0600

    Update knife_solo.rb

--- recipes/knife_solo.rb
+++ recipes/knife_solo.rb
@@ -1,5 +1,5 @@
 
 execute 'knife_solo' do 
-  command "sudo su chef -l -c 'sudo /opt/chefdk/bin/chef gem uninstall knife-solo && sudo rm -rf /home/chef/.chefdk/gem/ruby/2.1.0/gems/knife-solo-* && sudo su chef -l -c '/opt/chefdk/bin/chef gem install knife-solo'"
+  command "sudo rm -rf /home/chef/.chefdk/gem/ruby/2.1.0/gems/knife-solo-* && sudo su chef -l -c '/opt/chefdk/bin/chef gem install knife-solo'"
 end 
 
