8ab1cd2d67cb7c810fa688e39bf53f22f148d53b
Renlor <jvandebr@gmail.com>
2015-10-04 16:42:55 -0600

    Update knife_solo.rb

--- recipes/knife_solo.rb
+++ recipes/knife_solo.rb
@@ -1,7 +1,7 @@
 
 execute 'knife_solo' do 
   command 'sudo rm -rf /home/chef/.chefdk/gem/ruby/2.1.0/gems/knife-solo-*'
-  not_if { ::File.writeable?('/home/chef/.chefdk/gem/ruby/2.1.0/gems/knife-solo-0.5.1/CHANGELOG.md') }
+  not_if { ::File.writable?('/home/chef/.chefdk/gem/ruby/2.1.0/gems/knife-solo-0.5.1/CHANGELOG.md') }
 end 
 
 chef_gem 'knife-solo'
